7b2e131b9652e14c26a4d072883162b7482ac088
Bryce <swarvingprogrammer@gmail.com>
2021-08-18 20:47:48 -0400

    Add matching username and passwords method.

--- tests/test_wilbur.py
+++ tests/test_wilbur.py
@@ -12,6 +12,7 @@ from wilbur import (
     get_password_length,
     password_complexity,
     password_reuse,
+    username_password_match,
 )
 
 
@@ -27,11 +28,11 @@ class TestWilbur:
                 "password": "",
             }
         )
-        assert len(clean_empty_password(match_list)) == 9
+        assert len(clean_empty_password(match_list)) == 10
 
     def test_get_password_length(self, match_list):
         """Test the get password length method."""
-        assert get_password_length(match_list) == {8: 4, 3: 2, 12: 1, 6: 1, 11: 1}
+        assert get_password_length(match_list) == {8: 4, 3: 2, 12: 1, 6: 1, 11: 1, 5: 1}
 
     def test_password_reuse(self, match_list):
         """Test the password reuse method."""
@@ -45,4 +46,14 @@ class TestWilbur:
 
     def test_password_complexity(self, match_list):
         """Test the password complexity method."""
-        assert password_complexity(match_list) == {1: 1, 2: 1, 3: 1, 4: 2}
+        assert password_complexity(match_list) == {1: 2, 2: 1, 3: 1, 4: 2}
+
+    def test_username_password_match(self, match_list):
+        """Test the username password match method."""
+        assert username_password_match(match_list) == [
+            {
+                "user": "admin",
+                "hash": "21232f297a57a5a743894a0e4a801fc3",
+                "password": "admin",
+            },
+        ]
